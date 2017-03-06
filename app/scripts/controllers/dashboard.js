81a4a6b9a3508a80863b2360fa50669df7e2d529
rhsbalaram <rhsbalaram@gmail.com>
2017-03-06 17:34:18 +0530

    modificcation on filters

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -765,72 +765,7 @@ console.log("----------->"+JSON.stringify($scope.chartOptions));
   ///////////for charts data
         // Sample options for first chart
         /////
- $scope.chartOptions =  {
-  colors: ['#1f5dea', '#43cb83', '#c60000', '#ff9000'],
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: '<b>Event Location wise and Status wise</b>'
-    },
-    subtitle: {
-        text: 'GHMC'
-    },
-    xAxis: {
-        categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Greviences(count)'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y}</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        name: 'Pending',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-    }, {
-        name: 'Approved',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-    }, {
-        name: 'Rejected',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-        name: 'Submitted',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-    }]
-};
+ $scope.chartOptions =  {};
 //////
      $scope.currentdate=new Date();       
 
